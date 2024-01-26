cea008f7246627ab39c69dd272a2793b0c53132e
thinhta <quocthinh1992002@gmail.com>
2024-01-27 00:28:43 +0700

    init project

---
+++ src/main/java/com/bohaohan/leagueoflegends/LeagueoflegendsBeApplication.java
@@ -0,0 +1,13 @@
+package com.bohaohan.leagueoflegends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueoflegendsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueoflegendsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohaohan/leagueoflegends/LeagueoflegendsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohaohan.leagueoflegends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueoflegendsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
